2fb5c922867dd5041bd738d26e1b6e770164178e
Kidiloska <106271793+rosebakthan08@users.noreply.github.com>
2022-07-29 09:58:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 @pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
 async def start(client: pr0fess0r_99, message: Message):
     approvedbot = await client.get_me() 
-    button = [[ InlineKeyboardButton("Devoloper 🖥️", url="https://github.com/PR0FESS0R-99/Auto-Approved-Bot"), InlineKeyboardButton("Updates ", url="t.me/rosebakthan") ],
+    button = [[ InlineKeyboardButton("Devoloper 🖥️", url="https://t.me/dragadm"), InlineKeyboardButton("Updates ", url="t.me/rosebakthan") ],
               [ InlineKeyboardButton(" Add Me To Your Chat ", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
     await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart) || Repo https://github.com/PR0FESS0R-99/Auto-Approved-Bot||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
